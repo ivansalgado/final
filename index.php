45ae0e3194d94dd098962ca965e1eb859eb56225
Ivan Salgado <ivansalgado2000@gmail.com>
2015-05-20 08:33:32 -0700

    fixes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="shortcut icon" type="image/x-icon" href="ico/g.ico">
+        <link rel="shortcut icon" type="image/x-icon" href="ico/p.ico">
         <link type='text/css' rel='stylesheet' href='css/bootstrap-theme.css'>
         <link type='text/css' rel='stylesheet' href='css/bootstrap.css'>
         <link type='text/css' rel='stylesheet' href='css/custom-css.css'>
@@ -10,12 +10,12 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/lids/jqueryui/1.11.2/themes/smoothness/juery-ui.css" />
-        <title>Untitled</title>
+        <title>the phs experience</title>
         <style>
-            body {width:100%;}
+            body {margin: 0px;}
             #bg{width: 100%;}
-            #content{position:absolute; top:0px; padding-top:50px; margin:10px; width: 100%; margin-top:150px;}
-            #bg_container{height:598px; overflow:hidden;}
+            #content{position:absolute; top:0px; padding-top:50px; width: 1366px; margin-top:170px;}
+            #bg_container{height:615px; overflow:hidden;}
         </style>
     </head>
     <body>
@@ -26,18 +26,18 @@
         
         
         <div id="bg_container">
-        <video id="bg" src="video/sb.mp4" autoplay="true" loop="true" muted="true"></video>
+            <video id="bg" src="video/tropico.mp4" autoplay="true" loop="true" muted="true"></video>
         </div>
         
     <center>
         <div id="content">
-            <div id="accordion" class="row">
-            <h6 id="1" class="row">p</h6>
-            <p class="row">pasadena high school</p>
-            <h6 id="2" class="row">h</h6>
-            <p class="row"> these are more words</p>
-            <h6 id="3" class="row">s</h6>
-            <p class="row"><a href="stories.html"> this is a link</a></p>
+            <div id="accordion">
+            <h6 id="1">p</h6>
+            <p>pasadena high school</p>
+            <h6 id="2">h</h6>
+            <p>hear our stories</p>
+            <h6 id="3">s</h6>
+            <p><a href="stories.html">see who we are </a></p>
         </div>
         </div>
     </center>
